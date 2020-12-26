a4500966218a7318ea1b515df3ea194e23e8a275
YinWin <yinweitaishan@hotmail.com>
2020-12-26 16:12:56 +0800

    aa

--- t61.py
+++ t61.py
@@ -1,9 +1,11 @@
 l = [1,1]
+print([1])
+print(l)
 for i in range(2,10):
     
     m = [1]
     for j in range(i-1):
-        print(l[j+1])
+        # print(l[j+1])
         l[j] = l[j] +l[j+1]
     l = m + l 
     print(l)
