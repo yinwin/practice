f074f7562c25cd44a8abdce8b4ba54d5989c38c2
YinWin <yinweitaishan@hotmail.com>
2021-01-13 14:59:30 +0800

    gg

--- tt1.py
+++ tt1.py
@@ -1,19 +1,11 @@
-from sys import stdout
-if __name__ == '__main__':
-    a = int(input('输入四个数字:\n'))
-    aa = []
-    aa.append(a % 10)
-    aa.append(a % 100 // 10)
-    aa.append(a % 1000 // 100)
-    aa.append(a // 1000)
-    print(aa)
- 
-    for i in range(4):
-        aa[i] += 5
-        aa[i] %= 10
-    print(aa)
-    for i in range(2):
-        aa[i],aa[3 - i] = aa[3 - i],aa[i]
-    print(aa)
-    for i in range(3,-1,-1):
-        stdout.write(str(aa[i]))
+a='abcdada'
+b='a'
+c=[]
+d=-1
+while a.find(b) != -1:
+    d += a.find(b)+1
+    c.append(d)
+    a = a[a.find(b)+1:]
+    
+print(c)
+    
